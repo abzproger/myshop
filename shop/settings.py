91e41822290225a34000e8f01ebae131b35c5575
Abzaydin <abzaydin.1@gmail.com>
2025-12-04 05:19:07 +0300

    apps: add catalog, cart, orders, users and update settings

--- shop/settings.py
+++ shop/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog',
+    'cart',
+    'orders',
+    'users',
 ]
 
 MIDDLEWARE = [
